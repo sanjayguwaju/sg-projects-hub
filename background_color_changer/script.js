06826590313f476feb73139b412fba03b3503f3a
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-01 20:49:58 +0545

    project: background color changer

---
+++ background_color_changer/script.js
@@ -0,0 +1,14 @@
+const saroj = {
+    name: 'Saroj',
+    age: 23,
+    greet: function() {
+        console.log('Hey, its i am baklol');
+    },
+    friends: ["hari", "shyam", "gita"]
+    // 
+}
+
+
+console.log(saroj.name);
+console.log(saroj.greet())
+console.log(saroj.friends[0])
