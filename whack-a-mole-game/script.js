fd4a632ae2b78da211b1eaa16be97429c68e45ae
sanjayguwaju <sanjayguwaju@gmail.com>
2024-06-06 16:44:34 +0545

    fixed: code formatting completed

--- whack-a-mole-game/script.js
+++ whack-a-mole-game/script.js
@@ -3,22 +3,22 @@ let score = 0;
 let mole;
 
 function showMole() {
-  const randomHole = holes[Math.floor(Math.random() * holes.length)];
-  randomHole.classList.add('mole');
-  mole = randomHole;
-  setTimeout(() => {
-    randomHole.classList.remove('mole');
-    showMole();
-  }, 1000);
+    const randomHole = holes[Math.floor(Math.random() * holes.length)];
+    randomHole.classList.add('mole');
+    mole = randomHole;
+    setTimeout(() => {
+        randomHole.classList.remove('mole');
+        showMole();
+    }, 1000);
 }
 
 holes.forEach(hole => {
-  hole.addEventListener('click', function () {
-    if (this === mole) {
-      score++;
-      document.getElementById('score').textContent = score;
-    }
-  });
+    hole.addEventListener('click', function () {
+        if (this === mole) {
+            score++;
+            document.getElementById('score').textContent = score;
+        }
+    });
 });
 
 showMole();
