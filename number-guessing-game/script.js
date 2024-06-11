953bd287c0e729063438b7134b4220bda9333ccd
sanjayguwaju <sanjayguwaju@gmail.com>
2024-06-11 16:47:44 +0545

    fixed: code formatting and implemented the logic

--- number-guessing-game/script.js
+++ number-guessing-game/script.js
@@ -1,13 +1,13 @@
 const numberToGuess = Math.floor(Math.random() * 100) + 1;
-    const message = document.getElementById('message');
+const message = document.getElementById('message');
 
-    document.getElementById('submit').addEventListener('click', () => {
-      const userGuess = Number(document.getElementById('guess').value);
-      if (userGuess === numberToGuess) {
+document.getElementById('submit').addEventListener('click', () => {
+    const userGuess = Number(document.getElementById('guess').value);
+    if (userGuess === numberToGuess) {
         message.textContent = "Congratulations! You guessed the correct number!";
-      } else if (userGuess > numberToGuess) {
+    } else if (userGuess > numberToGuess) {
         message.textContent = "Too high! Try again.";
-      } else {
+    } else {
         message.textContent = "Too low! Try again.";
-      }
-    });
+    }
+});
